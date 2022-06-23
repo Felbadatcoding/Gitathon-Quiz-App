ad965cd2910dac980dedfef859292353ef69e194
Felix <felix_f_dimjati@s>
2022-06-23 14:15:19 +0800

    Initial Commit

---
+++ Gitathon Quiz App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Gitathon Quiz App
+//
+//  Created by Felix Forbes Dimjati on 23/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gitathon Quiz App/Gitathon_Quiz_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gitathon_Quiz_AppApp.swift
+//  Gitathon Quiz App
+//
+//  Created by Felix Forbes Dimjati on 23/6/22.
+//
+
+import SwiftUI
+
+@main
+struct Gitathon_Quiz_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
